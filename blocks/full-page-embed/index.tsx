5cfb5b5fb94ea78dcb5931c0ff2229879c11f629
Todd Moy <todd.moy@gmail.com>
2023-02-08 10:54:26 -0700

    Stuff

---
+++ blocks/full-page-embed/index.tsx
@@ -0,0 +1,16 @@
+import { FileBlockProps, getLanguageFromFilename } from "@githubnext/blocks";
+import { Button, Box } from "@primer/react";
+import "./index.css";
+
+export default function (props: FileBlockProps) {
+  const { context, content, metadata, onUpdateMetadata } = props;
+  const language = Boolean(context.path)
+    ? getLanguageFromFilename(context.path)
+    : "N/A";
+
+  return (
+    <>
+        <iframe width="100%" height="100%" style={{"border": 0}} src={content}></iframe>
+    </>
+  );
+}
