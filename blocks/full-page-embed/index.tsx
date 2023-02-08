747558cf23ed3470d93870b2e8a14bf6502954c5
Todd Moy <todd.moy@gmail.com>
2023-02-08 11:11:43 -0700

    Cleanup

--- blocks/full-page-embed/index.tsx
+++ blocks/full-page-embed/index.tsx
@@ -1,6 +1,4 @@
-import { FileBlockProps, getLanguageFromFilename } from "@githubnext/blocks";
-import { Button, Box } from "@primer/react";
-import "./index.css";
+import { FileBlockProps, getLanguageFromFilename } from "@githubnext/blocks"
 
 export default function (props: FileBlockProps) {
   const { context, content, metadata, onUpdateMetadata } = props;
@@ -9,8 +7,6 @@ export default function (props: FileBlockProps) {
     : "N/A";
 
   return (
-    <>
-        <iframe width="100%" height="100%" style={{"border": 0}} src={content}></iframe>
-    </>
+    <iframe width="100%" height="100%" style={{"border": 0}} src={content}></iframe>
   );
 }
